ccd54eebdc27b7bdcadfe01861a846491a1c6b86
Abhishek0706 <apatil@cs.iitr.ac.in>
2019-10-16 07:17:55 +0530

    added some comments

--- app/src/main/java/com/mdg/weather/MainActivity.java
+++ app/src/main/java/com/mdg/weather/MainActivity.java
@@ -9,10 +9,13 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    // Api key to access data fron an api like openWeatherApi
     private static final String APIKEY = "5753db64c667d9ed89962813328c19be";
 
-    //
+    //Declaring an object of APIInterface
 
+
+    //Defining all the views that are in xml file activity_main.xml
     TextView pressure, temp, wind, desc, cityName;
     EditText editText;
     Button submit;
@@ -22,20 +25,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //finding all the views from xml file
+        //made a method below to keep the code clean
         instantiateViews();
 
-        //
+        //Creating the object of APIInterface that we have created earlier, using ApiClient
+
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //getting the data from the edittext
                 String zipCode = editText.getText().toString();
+                //clearing the data from edittext after getting it into a variable
                 editText.getText().clear();
+                //fetching data according to the entered zip code
                 fetchData(zipCode);
             }
         });
     }
 
+    //method to instantiate all the views
     private void instantiateViews() {
         pressure = findViewById(R.id.pressure);
         temp = findViewById(R.id.temperature);
@@ -47,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    //method that fetch the data using api
     private void fetchData(String zipCode) {
 
 
